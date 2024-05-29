ad1541ce5e9cec6f9b1cb9971e810c04d2646a92
srdtrk <srdtrk@hotmail.com>
2024-05-29 17:18:25 +0200

    imp: added CLIENTS state

--- contracts/ics02-client/src/types/state.rs
+++ contracts/ics02-client/src/types/state.rs
@@ -1,6 +1,10 @@
 //! This module defines the state storage of the Contract.
 
-use cw_storage_plus::Item;
+use cosmwasm_std::Addr;
+use cw_storage_plus::{Item, Map};
 
 /// `NEXT_CLIENT_NUM` is the item that stores the next client number.
 pub const NEXT_CLIENT_NUM: Item<u64> = Item::new("client_num");
+
+/// `CLIENTS` is the map of all client ids to their contract address.
+pub const CLIENTS: Map<String, Addr> = Map::new("clients");
