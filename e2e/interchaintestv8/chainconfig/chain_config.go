012d40a29eae434e76630b1be966953f65aa2ab9
srdtrk <srdtrk@hotmail.com>
2024-06-13 15:52:12 +0400

    imp(e2e): using ibc-lite image as counterparty

--- e2e/interchaintestv8/chainconfig/chain_config.go
+++ e2e/interchaintestv8/chainconfig/chain_config.go
@@ -30,7 +30,7 @@ var DefaultChainSpecs = []*interchaintest.ChainSpec{
 			NoHostMount:    false,
 		},
 	},
-	// -- IBC-Go --
+	// -- IBC-Go with IBC-lite --
 	{
 		ChainConfig: ibc.ChainConfig{
 			Type:    "cosmos",
@@ -39,7 +39,7 @@ var DefaultChainSpecs = []*interchaintest.ChainSpec{
 			Images: []ibc.DockerImage{
 				{
 					Repository: "ghcr.io/cosmos/ibc-go-simd", // FOR LOCAL IMAGE USE: Docker Image Name
-					Version:    "v8.3.0",                     // FOR LOCAL IMAGE USE: Docker Image Tag
+					Version:    "pr-6299",                    // FOR LOCAL IMAGE USE: Docker Image Tag
 					UidGid:     "1025:1025",
 				},
 			},
