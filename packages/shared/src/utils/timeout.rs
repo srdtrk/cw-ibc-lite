1847dd1e06a82ceaaba5a5b20be4c29106e71978
srdtrk <59252793+srdtrk@users.noreply.github.com>
2024-07-30 12:00:23 +0200

    imp: added recv_packet timeout check (#12)
    * imp: recv packet timeout check
    
    * imp: timeout
    
    * imp: fix timeout

--- packages/shared/src/utils/timeout.rs
+++ packages/shared/src/utils/timeout.rs
@@ -1,6 +1,9 @@
 //! This module contains helper functions for working with [`cosmwasm_std::IbcTimeout`].
 
+use std::str::FromStr;
+
 use cosmwasm_std::{Env, IbcTimeout};
+use ibc_core_host::types::identifiers::ChainId;
 
 use crate::types::error::ContractError;
 
@@ -9,14 +12,33 @@ use crate::types::error::ContractError;
 /// # Errors
 /// Returns an error if the timeout is not a timestamp or if the timestamp is in the past.
 pub fn validate(env: &Env, timeout: &IbcTimeout) -> Result<(), ContractError> {
-    if timeout.block().is_some() {
-        return Err(ContractError::InvalidTimeoutHeight);
-    }
+    timeout.block().map_or(Ok(()), |b| {
+        if b.height == 0 && b.revision == 0 {
+            // This is a special case where the timeout is not set.
+            return Ok(());
+        }
+
+        if env.block.height >= b.height {
+            return Err(ContractError::invalid_timeout_block(
+                env.block.height,
+                b.height,
+            ));
+        }
+
+        if ChainId::from_str(&env.block.chain_id)?.revision_number() > b.revision {
+            return Err(ContractError::invalid_revision_number(
+                env.block.height,
+                b.revision,
+            ));
+        }
+
+        Ok(())
+    })?;
     timeout
         .timestamp()
         .ok_or(ContractError::EmptyTimestamp)
         .and_then(|ts| {
-            if env.block.time > ts {
+            if env.block.time >= ts {
                 return Err(ContractError::invalid_timeout_timestamp(
                     env.block.time.seconds(),
                     ts.seconds(),
